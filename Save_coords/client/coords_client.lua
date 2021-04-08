357a4fc0b9d54767628ef8f0ec1110ca5cdfe3ba
SimoN <31354321+Simonsims3@users.noreply.github.com>
2021-04-09 00:14:16 +0200

    Update coords_client.lua

--- Save_coords/client/coords_client.lua
+++ Save_coords/client/coords_client.lua
@@ -67,3 +67,8 @@ AddEventHandler('EndOfSave', function()
         args = {'Coords ', 'Everything is saved'}
     })
 end)
+
+function round(num, numDecimalPlaces)
+    local mult = 10^(numDecimalPlaces or 0)
+    return math.floor(num * mult + 0.5) / mult
+end
