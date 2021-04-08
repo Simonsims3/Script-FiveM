c383ac2f473b3a89dab6df363872f12b6b40e437
SimoN <31354321+Simonsims3@users.noreply.github.com>
2021-04-09 00:14:36 +0200

    Update coords_server.lua

--- Save_coords/server/coords_server.lua
+++ Save_coords/server/coords_server.lua
@@ -14,3 +14,8 @@ AddEventHandler('saveCoords', function(coords)
 
     TriggerClientEvent('EndOfSave', _source)
 end)
+
+function round(num, numDecimalPlaces)
+    local mult = 10^(numDecimalPlaces or 0)
+    return math.floor(num * mult + 0.5) / mult
+end
